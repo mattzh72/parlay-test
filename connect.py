d43b83b36a4ac99cad89a3a4430176e8f5337e4f
Matt Zhou <mattzh72@mgmail.com>
2021-06-01 17:31:19 -0700

    Connect to Parlay RDS instance

--- connect.py
+++ connect.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import mysql.connector
+import sys
+import boto3
+import os
+
+ENDPOINT="parlay-scraped-1.cye7mkifplef.us-east-2.rds.amazonaws.com"
+PORT="3306"
+USR="admin"
+REGION="us-east-2c"
+DBNAME="parlay-scraped-1"
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+
+#gets the credentials from .aws/credentials
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+
+token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)
+
+try:
+    conn =  mysql.connector.connect(host=ENDPOINT, user=USR, passwd=token, port=PORT, database=DBNAME)
+    print("Database connection success.")
+except Exception as e:
+    print("Database connection failed due to {}".format(e))       
